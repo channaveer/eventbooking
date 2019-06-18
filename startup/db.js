6f61393ab80d6576040b6ae18302dd51ce9679c5
channaveer <channaveer888@gmail.com>
2019-06-18 12:44:56 +0530

    added auth source

--- startup/db.js
+++ startup/db.js
@@ -1,6 +1,6 @@
 const mongoose  = require('mongoose');
 mongoose.set('useCreateIndex', true);
 module.exports = function(){
-    mongoose.connect(process.env.DB_CONNECTION+"://"+ process.env.DB_USERNAME + ':'+ process.env.DB_PASSWORD + '@'  + process.env.DB_HOST +":"+ process.env.DB_PORT +"/"+process.env.DB_NAME, { useNewUrlParser: true })
+    mongoose.connect(process.env.DB_CONNECTION+"://"+ process.env.DB_USERNAME + ':'+ process.env.DB_PASSWORD + '@'  + process.env.DB_HOST +":"+ process.env.DB_PORT +"/"+process.env.DB_NAME + "?authSource=" + process.env.DB_AUTH_SOURCE, { useNewUrlParser: true })
         .then( () => console.log('Connected to mongodb successfully'));
 }
